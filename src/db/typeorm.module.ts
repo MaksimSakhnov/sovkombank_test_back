56f93c7f043a363427bc0439ae6ddc242e0be5b2
Максим Сахнов <maxim.sahnov@satellite-soft.ru>
2024-03-28 15:54:33 +0400

    upd docker

--- src/db/typeorm.module.ts
+++ src/db/typeorm.module.ts
@@ -5,17 +5,19 @@ import * as process from 'process';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 
+
 @Module({
   imports: [
     NestTypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.POSTGRES_HOST,
+      host: 'db',
       port: Number(process.env.POSTGRES_PORT),
       username: String(process.env.POSTGRES_USERNAME),
       password: String(process.env.POSTGRES_PASSWORD),
-      database: process.env.POSTGRES_DATABASE,
+      database: String(process.env.POSTGRES_DATABASE),
       entities: ['dist/modules/**/*.entity.js'],
       synchronize: true,
+      autoLoadEntities: true,
       // migrations: [ 'dist/db/migrations/**/*.js' ],
       // cli: { migrationsDir: 'src/db/migrations' },
     }),
